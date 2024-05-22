387990b96ee276560106ee3ea983db737cda7cbe
Jidne007 <163674890+Jidne007@users.noreply.github.com>
2024-05-22 23:33:14 +0600

    Add files via upload

---
+++ JS--class-3(rahulvi)/index.js
@@ -0,0 +1,157 @@
+var student = [1,45,5,9,9,7,4,5,94,20,10,15,52];
+
+var asif =50;
+var rohim =60;
+var jidne =30;
+var sami =10;
+
+/**Array er sahajje ek line a list ta likha jay..niche likha holo */
+
+
+
+
+// var studentTaka =[50,60,30,10];
+/**suchi potro ber kora */
+/**js a..0theke count shuru kore js..such as 0 1 2 3..  */
+// var index = studentTaka.indexOf(60)
+// console.log(index);
+
+
+
+
+/**Id nam ber korte hole */
+
+// console.log(studentTaka.length);
+
+/**length */
+
+
+// var studentname =['yasin','jidne','jayeed','sorif','rakin'];
+
+// console.log(studentname.length);
+
+
+// var taka =[20,30,10,52,82,92,43,61,52,43,90,70,40];
+//  var indexFind = taka[5];
+//  console.log(indexFind);
+
+
+ /**jokhn amra string likhbo amader obosshoi qotation use kora lagbe */
+
+// JODI ARRAY ER VALUE CHANGE KORTE CHAY BAA REPLACE KORTE CHAI TAHOLE...
+
+// var studentTaka = [50,60,40,10,59,10,30,20,87];
+// studentTaka[2] =100;
+// console.log(studentTaka);
+
+
+// JODI STRING ER VALUE BARAYTE CHAY TAHOLE...
+// var studentTaka = [50,60,40,10,59,10,30,20,87];
+
+// studentTaka.push(600);
+// console.log(studentTaka);
+
+// var sname = ['asif','fahim','rofik'];
+//  var addName =sname.push('babul');
+//  console.log(sname);
+//sesh a add hobe 600
+
+
+//AR JODI VALUE PROTHOM DIK THEKE BARAYTE CHAI TAHOLE..
+
+var sname = ['asif','fahim','rofik'];
+  // sname.unshift('jayeed');
+  // console.log(sname);
+
+
+  //JODI KONO VALUE BAT DITE BA DELETE KORTE CHAI(tobe nam likhte hoyna..js sesh dik theke delete kore dey) TAHOLE ..
+
+  // var deleteName = sname.pop()
+  // console.log(sname);
+
+
+
+  //JODI PROTHOM DIK THEKE VALUE BAT DITE CHAY TAHOLE..
+
+  // var sname = ['asif','fahim','rofik'];
+  // var deleteFirst = sname.shift()
+  // console.log(sname);
+
+
+  //JODI MAJKHAN THEKE KONO VALUE BAT DITE CHAI ..
+
+  // var studentArray = ['diba','komal','dibbiua','mati','chad'];
+
+  // studentArray.splice(2,1);
+  // console.log(studentArray);
+
+
+
+  /// EVEN NUMBER AND ODD NUMBER....
+
+
+  // var number = 5;
+
+  // if(number % 2 ==0){
+  //   console.log('This is Even Number')
+  // }else{
+  //   console.log('This is Odd Number')
+  // }
+
+
+
+
+  // var roll =654;
+
+  // if(roll % 2==0){
+  //   console.log('This is Even Number')
+  // }else{
+  //   console.log('This is Odd Number')
+  // }
+
+
+
+
+  // var age  =17;
+
+  // if(age>19){
+  //   console.log('Tomar biyer shomoy hoyeche')
+  // }else{
+  //   console.log('Tomar boyosh hoyni muri khaw')
+  // }
+
+
+
+
+  // var jobHolder = true;
+  // var monthlysalary =200000;
+  // var house = true;
+  // var handsome= true;
+         
+  // if(jobHolder == true && monthlysalary > 100000 && handsome == true && house==true){
+  //   console.log('Ami Tomake Biye korte chaii')
+  // }else{
+  //   console.log('jaw Muri Khaw onnokothaw giye')
+  // }
+
+
+
+
+  //example
+
+  var tour =['coxbazar','sajek','nepal','sajek']
+
+  if(tour[3]== 'nepal'){
+    console.log('Cholo nepal ghure ashi')
+  }else if(tour[1]== 'coxbazar'){
+    console.log('coxbajar jai cholo')
+  }else if (tour[1]=='sajek'){
+    console.log('cholo sajeeek jaii sobai')
+  }else if(tour[2]=='coxbajar'){
+    console.log('cholo sajek ghure ashi')
+
+  }else{
+    console.log('tataaa bye bye see you khatam gaya')
+  }
+
+
